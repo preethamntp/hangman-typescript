8de5e64e2912132f5b2268803b377d443b601b9d
preetham.ntp <preetham.ntp@nexergroup.com>
2023-04-25 18:52:33 +0530

    add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handler);
     };
-  }, [guessedLetters]);
+  }, [addGuessedLetter, guessedLetters]);
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
